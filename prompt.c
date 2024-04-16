4388edbcbc4ef726d3ddb20dd99d349dccb561f2
AxelNAY <axelnay@sfr.fr>
2024-04-16 14:01:19 +0000

    Complete the file prompt.c

---
+++ prompt.c
@@ -0,0 +1,26 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints "$ ", wait for the user to enter a command,
+ * prints it on the next line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+
+	printf("$ ");
+	while ((read = getline(&line, &len, stdin)) != -1)
+	{
+		printf("%s", line);
+	}
+	free(line);
+
+	return (0);
+}
