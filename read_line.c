f6c1d67e0b1ba00042774c05b77ea85e49ae5316
AxelNAY <axelnay@sfr.fr>
2024-04-16 09:54:40 +0000

    remove core files

--- read_line.c
+++ read_line.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
 
+/**
+ * main - prints "$ ", wait for the user to enter a command
+ * , prints it on the next line.
+ * @ac: number of items in av.
+ * @av: a NULL terminated array of strings.
+ * Return: Always 0.
+ */
+
+int main(int ac, char **av)
+{
+	char **arg = av;
+	arg++;
+	print ("$ ")
+	while (*arg != NULL)
+	{
+		printf("%s", *arg);
+		arg++;
+		if (*arg != NULL)
+			printf(", ");
+	}
+	printf("\n");
+
+	return (0);
+}
